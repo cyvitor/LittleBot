1591b9f477730ce91b26c0944bcf6602388619d3
cyvitor <cyvitorhugo@gmail.com>
2023-12-26 10:59:47 -0300

    ajustando todas as funções de banco para usar o pool

--- execQuery.js
+++ execQuery.js
@@ -34,73 +34,73 @@ async function execQuery2(query) {
 
 async function getOrdens() {
   const query = 'SELECT * FROM ordens where status IN (0, 1, 2)';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getOrdens2() {
   const query = 'SELECT o.id, o.symbol, o.side, o.type, o.quantity, o.price, o.leverage, o.status, o.target1, o.stopLoss, s.quantityPrecision, s.status as SymbolStatus  FROM ordens o, symbols s WHERE s.symbol=o.symbol AND o.status IN (0, 1, 2)';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function setOrderStateDone(id) {
   const query = 'UPDATE ordens SET status = 1 WHERE id = ' + id;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getAccs() {
   const query = 'SELECT * FROM accs where status = 1';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function saveAccOrder(accid, id, orderId, status, origQty, executedQty, type, side) {
   const query = `INSERT INTO accs_orders (acc_id, order_id, orderId, status, origQty, executedQty, type, side, datatime)VALUES(${accid}, ${id}, '${orderId}', '${status}', ${origQty}, ${executedQty}, '${type}', '${side}', now())`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function saveMsg(accid, msg, msg_code) {
   const query = `INSERT INTO acc_msg (acc_id, msg, msg_code, datatime)VALUES(${accid}, "${msg}", "${msg_code}", now())`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function setOrderStateClosed(id) {
   const query = 'UPDATE ordens SET status = 3 WHERE id = ' + id;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getAccOnOrder(id) {
   const query = 'SELECT o.acc_id as accid, o.order_id, o.origQty as quant, a.apiKey, a.apiSecret FROM accs_orders o, accs a WHERE o.acc_id=a.accid AND order_id = ' + id;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getOrdersProgrammed() {
   const query = 'SELECT * FROM ordens where status IN (4, 5)';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function setStartOrder(id, price) {
   const query = `UPDATE ordens SET status = 0, price = '${price}' WHERE id = ${id}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function setOrdersProgrammedClose(id) {
   const query = 'UPDATE ordens SET status = 5 WHERE id = ' + id;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function setStopOrder(id, price) {
   const query = `UPDATE ordens SET status = 2, price = ${price} WHERE id = ${id}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
@@ -127,13 +127,13 @@ async function updateBalance(accid, asset, balance, availableBalance) {
 async function updateAccInvestiment(accid, investment) {
   const invest = Math.floor(investment * 100) / 100;
   const query = `UPDATE accs SET investment = '${invest}' WHERE accid = ${accid}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function clearSymbols() {
   const query = "TRUNCATE symbols;"
-  const result = await execQuery(query);
+  const result = await execQuery2(query);
   return result;
 }
 
@@ -157,25 +157,25 @@ async function getAllBdSymbols() {
 
 async function updatePrice(id, price) {
   const query = `UPDATE ordens SET price = ${price} WHERE id = ${id}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getAcc(id) {
   const query = 'SELECT * FROM accs where accid = ' + id;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado[0];
 }
 
 async function getFirstAcc() {
   const query = 'SELECT * FROM accs WHERE status = 1 LIMIT 1';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado[0];
 }
 
 async function getNewOrders() {
   const query = 'SELECT o.accs_orders_id, o.orderId, s.symbol, o.status, a.accid, a.apiKey, a.apiSecret FROM accs_orders o, ordens s, accs a WHERE s.id=o.order_id AND a.accid=o.acc_id AND a.status = 1 AND o.status <> "FILLED"';
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
@@ -228,7 +228,7 @@ async function saveAccOrderOpen(accid, orderId, symbol, status, price, avgPrice,
 
 async function check_orders_open(orderId) {
   const query = `SELECT COUNT(*) AS count FROM accs_orders_open WHERE order_id = ${orderId}`;
-  const result = await execQuery(query);
+  const result = await execQuery2(query);
   return result[0].count > 0;
 }
 
@@ -259,7 +259,7 @@ async function saveAccPosition(accid, symbol, unrealizedProfit, leverage, entryP
       '${askNotional}'
     )`;
 
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
@@ -272,7 +272,7 @@ async function checkAccPosition(accid, symbol, entryPrice, positionAmt) {
       AND entryPrice = ${entryPrice}
       AND positionAmt = '${positionAmt}'`;
 
-  const result = await execQuery(query);
+  const result = await execQuery2(query);
 
   // Se houver registros, retorna o primeiro encontrado; caso contrário, retorna false.
   return result.length > 0 ? result[0] : false;
@@ -287,25 +287,25 @@ async function updateAccPosition(id, unrealizedProfit, bidNotional, askNotional)
       askNotional = '${askNotional}'
     WHERE id = ${id}`;
 
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function deleteAccPositions(id) {
   const query = `DELETE FROM accs_positions WHERE id = ${id}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function clearAccPositions(accid) {
   const query = `DELETE FROM accs_positions WHERE acc_id = ${accid}`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
 async function getPositionToClose() {
   const query = `SELECT p.id, p.symbol, p.positionSide, p.positionAmt, a.apiKey, a.apiSecret FROM accs_positions p, accs a WHERE p.acc_id=a.accid AND p.bot_status = 2`;
-  const resultado = await execQuery(query);
+  const resultado = await execQuery2(query);
   return resultado;
 }
 
